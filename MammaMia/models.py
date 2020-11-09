7dfb56737ec440188a3a24cd00c4ae18c923245e
Amaia Acha <50750904+amaiaacha@users.noreply.github.com>
2020-11-09 17:52:59 +0100

    crear2.0

---
+++ MammaMia/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+
+
+class Masa(models.Model):
+    nameMa = models.CharField(max_length = 255)
+    image = models.ImageField(upload_to ='media/imgMasa')
+    
+    def getMasa(self):
+        return reverse('masaPage', args=[self.pk])
+
+class Ingrediente(models.Model):
+    nameIn =models.CharField(max_length = 500)
+
+class Pizza(models.Model):
+    namePi = models.CharField(max_length = 255)
+    image = models.ImageField(upload_to = 'media/imgPizza')
+    masa = models.ForeignKey(Masa, on_delete = models.CASCADE)
+    ingrediente = models.ForeignKey(Ingrediente, on_delete = models.CASCADE)
---
+++ MammaMia/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Masa, Pizza
+
+
+# Create your views here.
+def homepage(request):
+    masas = Masa.objects.all()
+    context = {'masas':masas}
+    return render(request, 'home.html', context)
+
+def masaPage(request, pk):
+    masas = get_object_or_404(Masa, pk = pk)
+    pizzas = Pizza.objects.filter(masa=masas) #?
+    context = {'masas':masas, 'pizzas':pizzas}
+
+    return render(request, 'masaPage.html', context)
+
+
+
