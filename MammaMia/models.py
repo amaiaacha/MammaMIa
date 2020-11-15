e235780cd9d7b8e44dd4db139a7e1e3eec238b15
Amaia Acha <50750904+amaiaacha@users.noreply.github.com>
2020-11-15 20:04:04 +0100

    diseño mejorado y probando con manytomany

--- MammaMia/models.py
+++ MammaMia/models.py
@@ -3,19 +3,25 @@ from django.urls import reverse
 
 # Create your models here.
 
-
 class Masa(models.Model):
     nameMa = models.CharField(max_length = 255)
     image = models.ImageField(upload_to ='media')
-    
+    descripcion = models.CharField(max_length = 255)
+    pizzas = models.ManyToManyField(Pizza, on_delete = models.CASCADE)
+
     def getMasa(self):
         return reverse('masaPage', args=[self.pk])
-
+        
 class Ingrediente(models.Model):
     nameIn = models.CharField(max_length = 500)
+    pizzas = models.ManyToManyField(Pizza, on_delete = models.CASCADE)
 
 class Pizza(models.Model):
     namePi = models.CharField(max_length = 255)
     image = models.ImageField(upload_to = 'media')
-    masa = models.ForeignKey(Masa, on_delete = models.CASCADE)
-    ingrediente = models.ForeignKey(Ingrediente, on_delete = models.CASCADE)
+    masas = models.ManyToManyField(Masa, on_delete = models.CASCADE)
+    ingredientes = models.ManyToManyField(Ingrediente, on_delete = models.CASCADE)
+
+
+
+
